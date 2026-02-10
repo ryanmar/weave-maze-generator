1275f72118fb9785f176ab73ba7d5ec8ad56b983
Ryan Marshall <ryan@sevensoft.com>
2026-02-10 01:42:44 -0800

    feat: maze endpoint indication

--- src/main.ts
+++ src/main.ts
@@ -91,6 +91,10 @@ Dimensions (specify one only):
   -H, --image-height ...      Output image height in pixels
                               Default: (maze height x cell size) or (image width x maze height / maze width)
 
+Endpoints:                    In degrees clockwise from north (0, 360) or longest path if not specified. 
+  --entry-angle ...           Starting angle of maze entry point (default: longest path from exit)
+  --exit-angle ...            Angle of maze exit point (default: longest path from starting or overall)  
+
 Corners:
   -s, --square                Enables square corners instead of the default rounded corners.
 
@@ -162,6 +166,16 @@ async function main() {
                 key: 'maze-height',
                 flags: [ '-h', '--maze-height' ],
                 type: ParamType.INTEGER,
+            },
+            {
+                key: 'entry-angle',
+                flags: [ '--entry-angle' ],
+                type: ParamType.FLOAT,
+            },
+            {
+                key: 'exit-angle',
+                flags: [ '--exit-angle' ],
+                type: ParamType.FLOAT,
             },
             {
                 key: 'mask',
@@ -323,6 +337,10 @@ async function main() {
 
     let mazeWidth = args.get('maze-width') as number | undefined;
     let mazeHeight = args.get('maze-height') as number | undefined;
+
+    let entryAngle = args.get('entry-angle') as number | undefined;
+    let exitAngle = args.get('exit-angle') as number | undefined;   
+
     const maskFilename = args.get('mask') as string | undefined;
     let mask: boolean[][] | undefined;
     if (maskFilename) {
@@ -502,10 +520,14 @@ async function main() {
         return;
     }
 
-    void await saveMaze(generateMaze(new MazeOptions(mazeWidth, mazeHeight, loopsFrac, crossFrac, longPassages, mask, seed)),
-            new RenderOptions(outputDirectory, fileFormat, filenamePrefix, filenameSolutionSuffix, timestamp, solution,
-                    paperSize, cellSize, imageWidth, imageHeight, roundedCorners, lineWidthFrac, passageWidthFrac,
-                    wallColor, solutionColor, backgroundColor));
+    void await saveMaze(
+        generateMaze(
+            new MazeOptions(mazeWidth, mazeHeight, loopsFrac, crossFrac, longPassages, seed, mask, entryAngle, exitAngle)),
+        new RenderOptions(
+            outputDirectory, fileFormat, filenamePrefix, filenameSolutionSuffix, timestamp, solution,
+            paperSize, cellSize, imageWidth, imageHeight, roundedCorners, lineWidthFrac, passageWidthFrac,
+            wallColor, solutionColor, backgroundColor)
+        );
 }
 
 void await main();
--- src/maze/MazeOptions.ts
+++ src/maze/MazeOptions.ts
@@ -18,8 +18,10 @@ export class MazeOptions {
                 public readonly loopFrac = DEFAULT_LOOP_FRAC,
                 public readonly crossFrac = DEFAULT_CROSS_FRAC,
                 public readonly longPassages = DEFAULT_LONG_PASSAGES,
+                public readonly seed = Math.random()*2**32>>>0,
                 public readonly mask?: boolean[][],
-                public readonly seed = Math.random()*2**32>>>0
+                public readonly entryAngle?: number | undefined,
+                public readonly exitAngle?: number | undefined
                 ) {
     }
 }
